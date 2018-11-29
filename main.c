8a2e50dd065f660104e2bb47c6a8a5837ea96a99
PixLzzz <36031553+PixLzzz@users.noreply.github.com>
2018-11-29 17:11:28 +0100

    Partie 3 debut

--- main.c
+++ main.c
@@ -2,31 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 #define ACCOLADE '{'
-#define ACCOLADE_FERMANTE '}'
 #define CARRIAGERETURN '\n'
 #define SPACE ' '
 #define COMMA ','
+
 void arrayBracketEol();
 void operatorsSpacing();
+void noMultiDeclaration();
 int main()
 {
 
-    arrayBracketEol();
+    //arrayBracketEol();
     printf("\n");
-    operatorsSpacing();
+    //operatorsSpacing();
     printf("\n");
-    commaSpacing();
+    //commaSpacing();
+    printf("\n");
+    noMultiDeclaration();
 
     return 0;
 }
 
 
-void arrayBracketEol(){
+void arrayBracketEol()
+{
     FILE *fichier;
     fichier=NULL;
     fichier = fopen("test.c","rb");
-    char caractereActuel;
-    char caractereBuff;
+    char caractereActuel='0';
+    char caractereBuff='0';
     int numLine=1;
     if (fichier != NULL)
     {
@@ -54,16 +58,15 @@ void arrayBracketEol(){
     }
     else
     {
-        printf("Impossible d'ouvrir le fichier test.txt");
+        printf("Impossible d'ouvrir le fichier test.c");
 
     }
     fclose(fichier);
 
 }
 
-void operatorsSpacing(){
-
-    /* PENSER à GERER LES EXCEPTIONS EX:"* POUR LES DECLARATIONS DE POINTEURS"*/
+void operatorsSpacing()
+{
 
     FILE *fichier;
     fichier=NULL;
@@ -83,16 +86,15 @@ void operatorsSpacing(){
                 numLine++;
             }
             caractereActuel = fgetc(fichier);
-
-            if(43==caractereActuel || 42==caractereActuel || 47==caractereActuel || 45==caractereActuel || 61==caractereActuel || 63==caractereActuel || 58==caractereActuel){
-
-                fseek(fichier, -2, SEEK_CUR );
+            if(43==caractereActuel || 42==caractereActuel || 47==caractereActuel || 45==caractereActuel || 61==caractereActuel || 63==caractereActuel || 58==caractereActuel)
+            {
+                fseek(fichier, -1, SEEK_CUR );
                 prevChar=fgetc(fichier);
-                fseek(fichier, 1, SEEK_CUR);
+                fseek(fichier, 2, SEEK_CUR);
                 nextChar=fgetc(fichier);
-
-                if(prevChar!=SPACE || nextChar!=SPACE){
-                     printf("operators-spacing error  line %d\n",numLine);
+                if(prevChar!=SPACE ||nextChar!=SPACE)
+                {
+                    printf("operators-spacing error  line %d\n",numLine);
                 }
             }
 
@@ -104,13 +106,13 @@ void operatorsSpacing(){
         printf("Impossible d'ouvrir le fichier test.txt");
 
     }
-
     fclose(fichier);
 
 
 }
 
-void commaSpacing(){
+void commaSpacing()
+{
 
     FILE *fichier;
     fichier=NULL;
@@ -118,7 +120,6 @@ void commaSpacing(){
     char caractereActuel;
     char nextChar;
     int numLine=1;
-
 
 
     if (fichier != NULL)
@@ -130,11 +131,13 @@ void commaSpacing(){
                 numLine++;
             }
             caractereActuel = fgetc(fichier);
-            if(COMMA==caractereActuel){
+            if(COMMA==caractereActuel)
+            {
+                fseek(fichier, 1, SEEK_CUR );
                 nextChar=fgetc(fichier);
-
-                if(nextChar!=SPACE){
-                     printf("coma-spacing error  line %d\n",numLine);
+                if(nextChar!=SPACE)
+                {
+                    printf("coma-spacing error  line %d\n",numLine);
                 }
             }
 
@@ -148,44 +151,149 @@ void commaSpacing(){
     }
     fclose(fichier);
 
+
 }
 
-void indent(){
+
+
+void  noMultiDeclaration()
+{
 
     FILE *fichier;
     fichier=NULL;
     fichier = fopen("test.c","rb");
-    char caractereActuel;
-    char nextChar;
+    char a;
+    char buffVar;
     int numLine=1;
-
-
-
+    int checkCR=0;
+    int fileSize=0;
+    int nbCarac=0;
+    char buffer[6000];
     if (fichier != NULL)
     {
-        do
+       /* do
         {
-            if(CARRIAGERETURN==caractereActuel)
-            {
-                numLine++;
-            }
-            caractereActuel = fgetc(fichier);
-            if(COMMA==caractereActuel){
-                nextChar=fgetc(fichier);
+            a=fgetc(fichier);
+            nbCarac++;
+        }
+        while (!feof(fichier));
+         fileSize=nbCarac;
+        char buffer[fileSize];
+        printf("%d",fileSize);
+*/
+        do{
 
-                if(nextChar!=SPACE){
-                     printf("coma-spacing error  line %d\n",numLine);
+            for(int i=0; i<6000; i++) //On fait un buffer sous forme de chaques caractères pour enregistrer chaques lignes
+            {
+                buffer[i]=fgetc(fichier);
+
+                if(CARRIAGERETURN==buffer[i]){
+                    numLine++;
                 }
+                //printf("%c",buffer[i]);
+                //Debut du process pour trouver les types de variables
+                if('t'==buffer[i] && 'r'==buffer[i-1] && 'o'==buffer[i-2] && 'h'==buffer[i-3] && 's'==buffer[i-4] && (' '==buffer[i-5] || CARRIAGERETURN==buffer[i-5]))
+                {
+                    for(int y=3;y<33;y++){
+                        if(CARRIAGERETURN==buffer[i+y])
+                            checkCR=1;
+
+                        if (','==buffer[i+y] && checkCR==0)
+                            printf("no-multi-declaration error line %d",numLine);
+
+                        checkCR=0;
+                    }
+                }
+
+
+                if('e'==buffer[i] && 'l'==buffer[i-1] && 'b'==buffer[i-2] && 'u'==buffer[i-3] && 'o'==buffer[i] && 'd'==buffer[i-5] && (' '==buffer[i-6] || CARRIAGERETURN==buffer[i-6]))
+                {
+
+                    for(int y=3;y<33;y++){
+                        if(CARRIAGERETURN==buffer[i+y])
+                            checkCR=1;
+
+                        if (','==buffer[i+y] && checkCR==0)
+                            printf("no-multi-declaration error line %d",numLine);
+
+                        checkCR=0;
+                    }
+                }
+
+
+                if('t'==buffer[i] && 'n'==buffer[i-1] && 'i'==buffer[i-2] && (' '==buffer[i-3] || CARRIAGERETURN==buffer[i-3]))
+                {
+
+                    for(int y=3;y<33;y++){
+                        if(CARRIAGERETURN==buffer[i+y]){
+                            checkCR=1;
+                        }
+
+                        if (','==buffer[i+y] && checkCR==0){
+                            printf("no-multi-declaration error line %d",numLine);
+                        }
+                        checkCR=0;
+                    }
+
+
+                }
+
+
+                if('t'==buffer[i] && 'a'==buffer[i-1] && 'o'==buffer[i-2] && 'l'==buffer[i-3] && 'f'==buffer[i-4] && (' '==buffer[i-5] || CARRIAGERETURN==buffer[i-5]))
+                {
+                    for(int y=3;y<33;y++){
+                        if(CARRIAGERETURN==buffer[i+y])
+                            checkCR=1;
+
+                        if (','==buffer[i+y] && checkCR==0)
+                            printf("no-multi-declaration error line %d",numLine);
+
+                        checkCR=0;
+                    }
+                }
+
+
+                if('r'==buffer[i] && 'a'==buffer[i-1] && 'h'==buffer[i-2] && 'c'==buffer[i-3] && (' '==buffer[i-4] || CARRIAGERETURN==buffer[i-4]))
+                {
+
+                    for(int y=3;y<33;y++){
+                        if(CARRIAGERETURN==buffer[i+y])
+                            checkCR=1;
+
+                        if (','==buffer[i+y] && checkCR==0)
+                            printf("no-multi-declaration error line %d",numLine);
+
+                        checkCR=0;
+                    }
+                }
+
+
+                if('n'==buffer[i] && 'a'==buffer[i-1] && 'e'==buffer[i-2] && 'l'==buffer[i-3] && 'o'==buffer[i-4] && 'o'==buffer[i-5] && 'b'==buffer[i-6] && (' '==buffer[i-7] || CARRIAGERETURN==buffer[i-7]))
+                {
+                    for(int y=3;y<33;y++){
+                        if(CARRIAGERETURN==buffer[i+y])
+                            checkCR=1;
+
+                        if (','==buffer[i+y] && checkCR==0)
+                            printf("no-multi-declaration error line %d",numLine);
+
+                        checkCR=0;
+                    }
+                }
+
+
+
             }
 
         }
+
         while (!feof(fichier));
     }
     else
     {
-        printf("Impossible d'ouvrir le fichier test.txt");
+        printf("Impossible d'ouvrir le fichier test.c");
 
     }
+    printf("%d",numLine);
     fclose(fichier);
-
 }
--- test.c
+++ test.c
@@ -8,26 +8,30 @@
 
 void arrayBracketEol();
 void operatorsSpacing();
+void noMultiDeclaration();
 int main()
 {
 
-    arrayBracketEol();
+    //arrayBracketEol();
     printf("\n");
-    operatorsSpacing();
+    //operatorsSpacing();
     printf("\n");
-    commaSpacing();
+    //commaSpacing();
+    printf("\n");
+    noMultiDeclaration();
 
     return 0;
 }
 
 
-void arrayBracketEol(){
-    FILE * fichier;
-    fichier = NULL;
-    fichier = fopen("test.c", "rb");
-    char caractereActuel;
-    char caractereBuff;
-    int numLine=1;
+void arrayBracketEol()
+{
+    FILE *fichier;
+    fichier=NULL;
+    fichier = fopen("test.c","rb");
+    char caractereActuel='0';
+    char caractereBuff='0';
+    int numLine,nomlpo=1;
     if (fichier != NULL)
     {
         do
@@ -54,14 +58,15 @@ void arrayBracketEol(){
     }
     else
     {
-        printf("Impossible d'ouvrir le fichier test.txt");
+        printf("Impossible d'ouvrir le fichier test.c");
 
     }
     fclose(fichier);
 
 }
 
-void operatorsSpacing(){
+void operatorsSpacing()
+{
 
     FILE *fichier;
     fichier=NULL;
@@ -81,13 +86,15 @@ void operatorsSpacing(){
                 numLine++;
             }
             caractereActuel = fgetc(fichier);
-            if(43==caractereActuel || 42==caractereActuel || 47==caractereActuel || 45==caractereActuel || 61==caractereActuel || 63==caractereActuel || 58==caractereActuel){
+            if(43==caractereActuel || 42==caractereActuel || 47==caractereActuel || 45==caractereActuel || 61==caractereActuel || 63==caractereActuel || 58==caractereActuel)
+            {
                 fseek(fichier, -1, SEEK_CUR );
                 prevChar=fgetc(fichier);
                 fseek(fichier, 2, SEEK_CUR);
                 nextChar=fgetc(fichier);
-                if(prevChar!=SPACE ||nextChar!=SPACE){
-                     printf("operators-spacing error  line %d\n",numLine);
+                if(prevChar!=SPACE ||nextChar!=SPACE)
+                {
+                    printf("operators-spacing error  line %d\n",numLine);
                 }
             }
 
@@ -99,18 +106,18 @@ void operatorsSpacing(){
         printf("Impossible d'ouvrir le fichier test.txt");
 
     }
-
     fclose(fichier);
 
 
 }
 
-void commaSpacing(){
+void commaSpacing()
+{
 
     FILE *fichier;
     fichier=NULL;
-    fichier = fopen("test.c", "rb");
-    char caractereActuel;
+    fichier = fopen("test.c","rb");
+    char caractereActuel,tutu;
     char nextChar;
     int numLine=1;
 
@@ -124,11 +131,13 @@ void commaSpacing(){
                 numLine++;
             }
             caractereActuel = fgetc(fichier);
-            if(COMMA==caractereActuel){
+            if(COMMA==caractereActuel)
+            {
                 fseek(fichier, 1, SEEK_CUR );
                 nextChar=fgetc(fichier);
-                if(nextChar!=SPACE){
-                     printf("coma-spacing error  line %d\n",numLine);
+                if(nextChar!=SPACE)
+                {
+                    printf("coma-spacing error  line %d\n",numLine);
                 }
             }
 
@@ -142,4 +151,99 @@ void commaSpacing(){
     }
     fclose(fichier);
 
+
+}
+
+
+
+void  noMultiDeclaration()
+{
+
+    FILE *fichier;
+    fichier=NULL;
+    fichier = fopen("test.c","rb");
+    char a;
+    char caractereActuel;
+    int numLine=1;
+    int nbCarac=0;
+    char buffer[1000];
+    if (fichier != NULL)
+    {
+        do
+        {
+            a=fgetc(fichier);
+            for(int i=0; i<500; i++) //On fait un buffer sous forme de chaques caractères pour enregistrer chaques lignes
+            {
+                if(CARRIAGERETURN==a)
+                {
+                    for(int y=0; y<500; y++)
+                    {
+                        buffer[y]='0';
+                    }
+
+                    numLine++;
+                }
+
+                buffer[i]=a;
+                printf("%c",buffer[i]);
+                //Debut du process pour trouver les types de variables
+                if('t'==buffer[i] && 'r'==buffer[i-1] && 'o'==buffer[i-2] && 'h'==buffer[i-3] && 's'==buffer[i-4])
+                {
+                    printf("%d",numLine);
+                    printf("sdfghjkjhgfdsq");
+                }
+
+
+                if('e'==buffer[i] && 'l'==buffer[i-1] && 'b'==buffer[i-2] && 'u'==buffer[i-3] && 'o'==buffer[i] && 'd'==buffer[i-5])
+                {
+                    printf("%d",numLine);
+                    printf("sdfghjkjhgfdsq");
+                }
+
+
+                if(116==buffer[i] && 110==buffer[i-1] && 105==buffer[i-2])
+                {
+                    printf("%d",numLine);
+                    printf("sdfghjkjhgfdsq");
+                }
+
+
+                if('t'==buffer[i] && 'a'==buffer[i-1] && 'o'==buffer[i-2] && 'l'==buffer[i-3] && 'f'==buffer[i-4])
+                {
+                    printf("%d",numLine);
+                    printf("sdfghjkjhgfdsq");
+                }
+
+
+                if('r'==buffer[i] && 'a'==buffer[i-1] && 'h'==buffer[i-2] && 'c'==buffer[i-3])
+                {
+                    printf("%d",numLine);
+                    printf("sdfghjkjhgfdsq");
+                }
+
+
+                if('n'==buffer[i] && 'a'==buffer[i-1] && 'e'==buffer[i-2] && 'l'==buffer[i-3] && 'o'==buffer[i-4] && 'o'==buffer[i-5] && 'b'==buffer[i-6])
+                {
+                    printf("%d",numLine);
+                    printf("sdfghjkjhgfdsq");
+                }
+
+
+
+            }
+
+
+            nbCarac++;
+
+        }
+
+        while (!feof(fichier));
+    }
+    else
+    {
+        printf("Impossible d'ouvrir le fichier test.c");
+
+    }
+    printf("%d",nbCarac);
+    fclose(fichier);
 }
